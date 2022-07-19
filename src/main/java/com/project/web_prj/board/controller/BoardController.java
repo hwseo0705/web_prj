b30335a5a04948ceaf7d193876d9bb1e85110874
hwseo0705 <julieseo0705@gmail.com>
2022-07-19 10:27:21 +0900

    [07.19.22] Delete Modify Board

--- src/main/java/com/project/web_prj/board/controller/BoardController.java
+++ src/main/java/com/project/web_prj/board/controller/BoardController.java
@@ -23,7 +23,7 @@ public class BoardController {
     public String list(Model model) {
         log.info("controller request /board/list GET!");
         List<Board> boardList = boardService.findAllService();
-        log.info("return data - {}", boardList);
+        log.debug("return data - {}", boardList);
 
         model.addAttribute("bList", boardList);
         return "board/board-list";
@@ -53,5 +53,30 @@ public class BoardController {
         log.info("controller request /board/write POST! - {}", board);
         boolean flag = boardService.saveService(board);
         return flag ? "redirect:/board/list" : "redirect:/";
+    }
+
+    // 게시물 삭제 요청
+    @GetMapping("/delete")
+    public String delete(Long boardNo) {
+        log.info("controller request /board/delete GET! - {}", boardNo);
+        return boardService.removeService(boardNo) ? "redirect:/board/list" : "redirect:/";
+    }
+
+    // 게시글 수정 화면 요청
+    @GetMapping("/modify")
+    public String modify(Long boardNo, Model model) {
+        log.info("controller request /board/modify GET! - bno: {}", boardNo);
+        Board board = boardService.findOneService(boardNo);
+        log.info("article found: {}", board);
+
+        model.addAttribute("board", board);
+        return "board/board-modify";
+    }
+
+    // 게시글 수정 처리 요청
+    @PostMapping("/modify")
+    public String modify(Board board) {
+        log.info("controller request /board/modify POST! - {}", board);
+        return boardService.modifyService(board) ? "redirect:/board/content/" + board.getBoardNo() : "redirect:/";
     }
 }
