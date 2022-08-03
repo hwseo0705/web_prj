929dac512e34375e19b3bff6b44fdc9e3dfb7b9d
hwseo0705 <julieseo0705@gmail.com>
2022-08-03 16:57:59 +0900

    [08.03.22] Member Sign-Out Etc.

--- src/main/java/com/project/web_prj/HomeController.java
+++ src/main/java/com/project/web_prj/HomeController.java
@@ -1,10 +1,13 @@
 package com.project.web_prj;
 
+import com.project.web_prj.member.domain.Member;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,8 +16,14 @@ import java.util.Map;
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
+    public String home(Model model, HttpSession session) {
         log.info("welcome page");
+
+//        Member loginUser = (Member) session.getAttribute("loginUser");
+//        if (loginUser == null) {
+//            model.addAttribute("login", false);
+//            model.addAttribute("login", true);
+//        }
         return "index";
     }
 
--- src/main/java/com/project/web_prj/member/controller/MemberController.java
+++ src/main/java/com/project/web_prj/member/controller/MemberController.java
@@ -15,6 +15,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 @Controller
 @Log4j2
 @RequiredArgsConstructor
@@ -54,17 +57,37 @@ public class MemberController {
 
     // 로그인 요청 처리
     @PostMapping("/sign-in")
-    public String signIn(LoginDTO inputData, RedirectAttributes ra) {
+    public String signIn(LoginDTO inputData, RedirectAttributes ra,
+                         HttpSession session) { // 세션정보 객체
         log.info("/member/sign-in POST - {}", inputData);
+        // log.info("session timeout : {}", session.getMaxInactiveInterval());
 
         // 로그인 서비스 호출
-        LoginFlag flag = memberService.login(inputData);
+        LoginFlag flag = memberService.login(inputData, session);
+
         if (flag == LoginFlag.SUCCESS) {
             log.info("login success!!!");
             return "redirect:/";
         }
 
         ra.addFlashAttribute("loginMsg", flag);
+        return "redirect:/member/sign-in";
+    }
+
+    // 쿠키는 서버에서 클라이언트에게 주고, 세션은 클라이언트에게 주지 않는다.
+    // 세션은 브라우저가 꺼지면 세션 정보도 없어짐, 쿠키는 남아있음
+
+    @GetMapping("/sign-out")
+    public String signOut(HttpSession session) {
+
+        if (session.getAttribute("loginUser") != null) {
+            // 1. 세션에서 정보를 삭제한다.
+            session.removeAttribute("loginUser");
+
+            // 2. 세션을 무효화한다.
+            session.invalidate();
+            return "redirect:/";
+        }
         return "redirect:/member/sign-in";
     }
 }
